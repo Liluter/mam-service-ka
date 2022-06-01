56073bd1d87efe3636b60d0a751f9c2ac1cb654f
Liluter <karolawdziewicz@gmail.com>
2022-06-01 03:54:31 +0200

    som cleaning in store and readme.md

--- src/store/reducers/globalReducer.js
+++ src/store/reducers/globalReducer.js
@@ -200,8 +200,6 @@ const initialState = [
 ];
 
 export const memReducer = (state = initialState, action) => {
-    // state.tablica.filter((e) => (e.title == "Mem 1") || (e.title == "Mem 2"));
-    // console.log(state.filter((e) => (e.title == action.payload )));
 
     switch (action.type) {
         case "UPVOTE": {
@@ -224,38 +222,3 @@ export const memReducer = (state = initialState, action) => {
             return state ;
     }
 }
-
-// const ADD_TODO = 'ADD_TODO'
-// const TODO_TOGGLED = 'TODO_TOGGLED'
-
-// export const addTodo = text => ({
-//   type: ADD_TODO,
-//   payload: { text, id: nanoid() }
-// })
-
-// export const todoToggled = id => ({
-//   type: TODO_TOGGLED,
-//   payload: id
-// })
-
-// export const todosReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case ADD_TODO:
-//       return state.concat({
-//         id: action.payload.id,
-//         text: action.payload.text,
-//         completed: false
-//       })
-//     case TODO_TOGGLED:
-//       return state.map(todo => {
-//         if (todo.id !== action.payload.id) return todo
-
-//         return {
-//           ...todo,
-//           completed: !todo.completed
-//         }
-//       })
-//     default:
-//       return state
-//   }
-// }
